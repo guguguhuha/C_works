165484b9b14a9513c6f7b758b3f5a6b56f5b40b5
guguguhuha <guguguhuha@qq.com>
2021-01-03 16:16:09 +0800

    调试

--- c_works/学校 c 实验/实验 1/填空 1.c
+++
@@ -1,7 +0,0 @@
-#include "stdio.h"
-main()
-{
-	char c1, c2;
-	c1 = 97;   c2 = 98;
-	printf("\t%c %c", c1, c2);
-}
--- c_works/学校 c 实验/实验 1/改错 2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-	main()
- {
-    int a,b;
-    scanf("%d %d", &a, &b);
-    a = (a + b)/2;
-    printf("The average is %d\n", a);
-
-}
