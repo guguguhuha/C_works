8759b33a6916fff1a8696f8febf10e7a6b95680f
guguguhuha <guguguhuha@qq.com>
2021-01-20 21:47:31 +0800

    练习

---
+++ c_works/字符变换打印.c
@@ -0,0 +1,51 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<Windows.h>
+#include<string.h>
+
+void print( char _2[])
+{
+	printf("%s\n", _2);
+	Sleep(500);
+	system("cls");
+}
+
+int main()//纯字母
+{
+	char _1[] = "guguguhuha";
+	char _2[] = "##########";
+	int left = 0;
+	int right = strlen(_2) - 1;
+	print(_2);
+
+	while (left <= right)
+	{
+		_2[left++] = _1[left];
+		_2[right--] = _1[right];
+		print(_2);
+	}
+	printf("%s", _2);
+	return 0;
+}
+
+//int main()//汉字
+//{
+//	char _1[] = "咕咕咕呼哈";
+//	char _2[] = "##########";
+//	int left = 0;
+//	int right = strlen(_2) - 1;
+//	print(_2);
+//
+//	while (left <= right)
+//	{
+//		_2[left++] = _1[left];
+//		_2[left++] = _1[left];
+//		_2[right--] = _1[right];
+//		_2[right--] = _1[right];
+//		print(_2);
+//	}
+//	printf("%s", _2);
+//	return 0;
+//}
+
