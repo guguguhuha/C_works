8bd7adbf89fe14d1dd09ebda425110e18217602c
guguguhuha <guguguhuha@qq.com>
2021-01-20 23:33:57 +0800

    练习

---
+++ c_works/关机小程序.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+
+int main()
+{
+	system("shutdown -s -t 60");
+	char input[20] = { 0 };
+	printf("请注意你的电脑将于一分钟内关机\n如果输入 我是猪 \n便可取消关机！\n");
+again:
+	scanf("%s", &input);
+	if (strcmp(input, "我是猪") == 0)
+	{
+		system("shutdown -a");
+		printf("恭喜你,在你的自我肯定下取消了关机！\n");
+	}
+	else
+	{
+		printf("哦豁！你不听话哦！\n");
+		goto again;
+	}
+
+	return 0;
+}
---
+++ c_works/猜数字游戏.c
@@ -0,0 +1,77 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void menu()
+{
+	printf("############\n");
+	printf("## 1.play ##\n");
+	printf("## 0.quit ##\n");
+	printf("############\n");
+}
+
+void play()
+{
+	int result = 0;
+	int ret = rand() % 100 + 1;
+	while (1)
+	{
+		printf("请输入你所猜的数字：");
+		scanf("%d", &result);
+		if (result > ret)
+		{
+			printf("猜大了！\n");
+		}
+		else if (result < ret)
+		{
+			printf("猜小了！\n");
+		}
+		else
+		{
+			printf("恭喜你，猜对了！\n");
+			break;
+		}
+	}
+}
+
+void quit()
+{
+	printf("谢谢使用！\n欢迎下次再来\n");
+	exit(0);
+}
+void start()
+{
+	int choice;
+	printf("请输入你的选择(1/0): ");
+	scanf("%d", &choice);
+	switch (choice)
+	{
+	case 1:
+		play();
+		break;
+	case 0:
+		quit();
+		break;
+	default:
+		printf("哦豁 ！\n");
+		break;
+	}
+}
+
+void game()
+{
+	srand((unsigned)time(NULL));
+	menu();
+	while (1)
+	{
+		start();
+	}
+}
+
+int main()
+{
+	game();
+	return 0;
+}
