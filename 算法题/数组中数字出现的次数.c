9ae8b36fbf10d8aa51f46ea65038bce2beb7920c
guguguhuha <guguguhuha@qq.com>
2021-03-30 23:01:08 +0800

    只出现一次的两个数

--- 算法题/数组中数字出现的次数.c
+++ 算法题/数组中数字出现的次数.c
@@ -15,28 +15,29 @@ int *singleNumbers(int *nums, int numsSize, int *returnSize) {
     int ret = 0;
     int i = 0;
     for (i = 0; i < numsSize; i++) {
-        ret ^= nums[i];
+        ret ^= nums[i];//将整个数组异或
     }
 
-    int j = 0;
+    int j = 0;//在异或最终的结果，肯定至少有一位不同，现在就在找那一位
     for (j = 0; j < 32; j++) {
         if (ret & ((unsigned int) 1 << j)) {
             break;
         }
     }
-
+    //分组，在那一位相同的分为一组，不同的分为一组，两组分别进行异或，最终抑或出只出现一次的数字，因为前面已经把这两个只出现一次的数字分开了，最终异或只剩下自己
     int k = 0;
     for (k = 0; k < numsSize; k++) {
         if (nums[k] & ((unsigned int) 1 << j)) {
+            //1一定要是无符号的（或者long long，不然当一向左移动31位，该怎么算，或者将数组元素右移也可以
             x ^= nums[k];
         } else {
             y ^= nums[k];
         }
     }
 
-    *returnSize = 2;
+    *returnSize = 2;//告诉后台返回了几个值
 
-    int *num = (int *) malloc(8);
+    int *num = (int *) malloc(8);//leetcode返回值要求，自己动态开辟一个内存
     num[0] = x;
     num[1] = y;
 
